ff13abb04b5c5447b72c7dfd1fc38939a6465435
Swiip <matthieu.lux@gmail.com>
2013-03-23 08:27:04 +0100

    new versions

--- server/app.js
+++ server/app.js
@@ -3,74 +3,74 @@ var monk = require('monk');
 var cors = require("./cors");
 
 var mongodbUrl = 'mongodb://swiip:password@linus.mongohq.com:10056/angular';
-var mongodbResource = 'carottes';
+var mongodbResource = 'frameworks';
 
 var db = monk(mongodbUrl);
-var carottes = db.get(mongodbResource);
+var frameworks = db.get(mongodbResource);
 
 var server = express();
 server.use(express.bodyParser());
 server.use(cors.express);
 
-server.all('/carottes', function (req, res) {
-    console.log('/carottes', req.method, req.body);
-    switch (req.method) {
-        case 'GET':
-            carottes.find({}, function (err, docs) {
-                res.send(docs);
-            });
-            break;
-        case 'PUT':
-            carottes.remove({}, function () {
-                carottes.insert(req.body, function () {
-                    res.send(200);
-                });
-            });
-            break;
-        case 'POST':
-            carottes.insert(req.body, function (err, doc) {
-                res.send(doc);
-            });
-            break;
-        case 'DELETE':
-            carottes.remove({}, function () {
-                res.send(200);
-            });
-            break;
-        default:
-            res.status(400).send('Method ' + req.method + 'not supported');
-    }
+server.all('/frameworks', function (req, res) {
+  console.log('/frameworks', req.method, req.body);
+  switch (req.method) {
+    case 'GET':
+      frameworks.find({}, function (err, docs) {
+        res.send(docs);
+      });
+      break;
+    case 'PUT':
+      frameworks.remove({}, function () {
+        frameworks.insert(req.body, function (err, doc) {
+          res.send(doc);
+        });
+      });
+      break;
+    case 'POST':
+      frameworks.insert(req.body, function (err, doc) {
+        res.send(doc);
+      });
+      break;
+    case 'DELETE':
+      frameworks.remove({}, function () {
+        res.send(200);
+      });
+      break;
+    default:
+      res.status(400).send('Method ' + req.method + 'not supported');
+  }
 });
 
-server.all('/carottes/:_id', function (req, res) {
-    console.log('/carottes/:_id', req.method, req.params, req.body);
-    switch (req.method) {
-        case 'GET':
-            carottes.findOne(req.params, function (err, doc) {
-                res.send(doc);
-            });
-            break;
-        case 'PUT':
-            if(req.body._id) {
-                delete req.body._id;
-            }
-            carottes.findAndModify({_id : req.params._id}, {$set: req.body}, function (err, doc) {
-                res.send(200);
-            });
-            break;
-        case 'POST':
-            carottes.updateById(req.params._id, req.body, function (err, doc) {
-                res.send(200);
-            });
-            break;
-        case 'DELETE':
-            carottes.remove(req.params, function () {
-                res.send(200);
-            });
-            break;
-        default:
-            res.status(400).send('Method ' + req.method + 'not supported');
-    }
+server.all('/frameworks/:_id', function (req, res) {
+  console.log('/frameworks/:_id', req.method, req.params, req.body);
+  switch (req.method) {
+    case 'GET':
+      frameworks.findOne(req.params, function (err, doc) {
+        res.send(doc);
+      });
+      break;
+    case 'PUT':
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      frameworks.findAndModify({_id: req.params._id}, {$set: req.body}, function (err, doc) {
+        res.send(doc);
+      });
+      break;
+    case 'POST':
+      frameworks.updateById(req.params._id, req.body, function (err, doc) {
+        res.send(doc);
+      });
+      break;
+    case 'DELETE':
+      frameworks.remove(req.params, function () {
+        res.send(200);
+      });
+      break;
+    default:
+      res.status(400).send('Method ' + req.method + 'not supported');
+  }
 });
 
 console.log('Starting pure REST handler for ' + mongodbUrl + ":" + mongodbResource);
